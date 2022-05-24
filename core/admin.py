cee0f3b743e9b31389a0b61944bcc2cfec748f9a
R & E Partnership <eleni@mailbox.org>
2022-05-24 16:51:38 +0530

    Change basic admin related values

---
+++ core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "Peling Chokhor Digital Library Administration"
+admin.site.site_title = "PCDL Admin"
+admin.site.index_title = "Library Administration"
