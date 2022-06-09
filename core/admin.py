879920ed5163635e013f237a090e809410793052
R & E Partnership <eleni@mailbox.org>
2022-06-09 12:21:00 +0530

    Make admin and main site look similar.

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-admin.site.site_header = "Peling Chokhor Digital Library Administration"
+admin.site.site_header = "Peling Chokhor Admin"
 admin.site.site_title = "PCDL Admin"
 admin.site.index_title = "Library Administration"
